667f9490c9b531ee751f327b7a4a976eacdbf511
Agnes <agnesajema@gmail.com>
2024-06-18 15:30:31 +0300

    add files

---
+++ real/src/Landing/index.js
@@ -0,0 +1,22 @@
+import './index.css'
+
+const Landing =()=>{
+    return(
+        <div id='whole'>
+        <nav id='navbar'>
+            <img src='/pics/logo.png' alt=''/>
+            <a href='#'>Home</a>
+            <a href='#'>About Us</a>
+            <a href='#'>Contacts</a>
+            </nav>
+            <div id='page'>
+                <p>Shop The Best Bales <br/>For Your Second Hand Clothes Business</p>
+            </div>
+        </div>
+        
+
+        
+
+    );
+}
+export default Landing
