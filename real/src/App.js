9a767f1e12535dcdef47a95e9b10820086abaf9e
Agnes <agnesajema@gmail.com>
2024-07-17 16:50:33 +0300

    add changes

--- real/src/App.js
+++ real/src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
+import Navbar  from './Navbar';
+import Landing from './Landing';
 import Product from './Product';
+
 
 
 function App() {
   return (
     <div>
+      <Navbar/>
+      <Landing/>
       <Product/>
 
     </div>
